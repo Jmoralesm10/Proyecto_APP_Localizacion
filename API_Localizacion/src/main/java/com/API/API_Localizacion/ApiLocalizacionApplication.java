2a52a89fb3ad9ebd9bc50389ab5250bd3f6c0ac1
Javier Morales <jmoralesm45@miumg.edu.gt>
2023-10-05 23:32:08 -0600

    Subiendo Proyecto API

---
+++ API_Localizacion/src/main/java/com/API/API_Localizacion/ApiLocalizacionApplication.java
@@ -0,0 +1,13 @@
+package com.API.API_Localizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLocalizacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLocalizacionApplication.class, args);
+	}
+
+}
---
+++ API_Localizacion/src/test/java/com/API/API_Localizacion/ApiLocalizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.API_Localizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLocalizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
